eda03bf62cff5fd41c5265f255947cb187e6bf18
Samasquini <114921819+Samasquini@users.noreply.github.com>
2022-10-20 14:47:50 -0300

    Update Program.cs

--- AppExemploGit/Program.cs
+++ AppExemploGit/Program.cs
@@ -1,2 +1,13 @@
-﻿Console.WriteLine("Ola, mundo!");
-Console.WriteLine("Ola, Matheus!");
+﻿Console.WriteLine("---------------------------");
+Console.WriteLine("[1] - Saudação.");
+Console.WriteLine("---------------------------");
+
+Console.Write("Escolha uma opção: ");
+var a = int.TryParse(Console.ReadLine(), out var b);
+if(b == 1)
+{
+    Console.Write("Qual é o seu nome: ");
+    string nome = Console.ReadLine();
+
+    Console.WriteLine($"Olá, {nome} !");
+}
